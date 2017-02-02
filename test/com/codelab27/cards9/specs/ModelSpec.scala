8232a0ea0671c010d240f742c7c3fe1af17e0620
Roberto Serrano <r.semarcos@gmail.com>
2017-02-02 21:29:20 +0100

    Recover destroyed Arrow and Specs changes

--- test/com/codelab27/cards9/specs/ModelSpec.scala
+++ test/com/codelab27/cards9/specs/ModelSpec.scala
@@ -3,4 +3,4 @@ package com.codelab27.cards9.specs
 import org.scalatest._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-abstract class ModelSpec extends WordSpec with Matchers with ShouldMatchers with GeneratorDrivenPropertyChecks
+abstract class ModelSpec extends WordSpec with Matchers with GeneratorDrivenPropertyChecks
