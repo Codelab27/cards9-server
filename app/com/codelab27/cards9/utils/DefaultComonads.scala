36a15d0b6a90ebdcac989802fe0fe7dd0c7f695d
Roberto Serrano <r.semarcos@gmail.com>
2017-12-27 19:35:10 +0100

    Add default Bimonad for Future

--- app/com/codelab27/cards9/utils/DefaultComonads.scala
+++ app/com/codelab27/cards9/utils/DefaultComonads.scala
@@ -1,6 +1,6 @@
 package com.codelab27.cards9.utils
 
-import cats.{CoflatMap, Comonad}
+import cats.{Bimonad, CoflatMap, Comonad, Monad}
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -15,6 +15,19 @@ object DefaultComonads {
     override def coflatMap[A, B](fa: Future[A])(f: Future[A] => B) = co.coflatMap(fa)(f)
 
     override def map[A, B](fa: Future[A])(f: A => B) = co.map(fa)(f)
+  }
+
+  implicit def futureBimonad(implicit m: Monad[Future], co: Comonad[Future]): Bimonad[Future] = new Bimonad[Future] {
+
+    override def tailRecM[A, B](a: A)(f: A => Future[Either[A, B]]) = m.tailRecM(a)(f)
+
+    override def flatMap[A, B](fa: Future[A])(f: A => Future[B]) = m.flatMap(fa)(f)
+
+    override def extract[A](x: Future[A]) = co.extract(x)
+
+    override def coflatMap[A, B](fa: Future[A])(f: Future[A] => B) = co.coflatMap(fa)(f)
+
+    override def pure[A](x: A) = m.pure(x)
   }
 
 }
